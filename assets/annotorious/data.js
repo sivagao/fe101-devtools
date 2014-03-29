d749f1463865abc6c1019cdcf781dbd56a11098f
gaohailang <gaohailang@wandoujia.com>
2014-03-29 14:27:31 +0800

    update

---
+++ assets/annotorious/data.js
@@ -0,0 +1,369 @@
+var AnnotationData = {
+    "results": [{
+        "context": "http://localhost:8000/#/4",
+        "shapes": [{
+            "geometry": {
+                "height": 0.06584362139917696,
+                "width": 0.061093247588424465,
+                "x": 0.19292604501607716,
+                "y": 0.08024691358024691
+            },
+            "type": "rect"
+        }],
+        "src": "http://localhost:8000/assets/material/dev-tools-diagram-overview.jpg",
+        "text": "不同的 Tab 对应不同的功能，通过鼠标切换功能模块\n",
+        "createdAt": "2014-03-23T12:19:37.176Z",
+        "updatedAt": "2014-03-23T14:49:16.602Z",
+        "objectId": "DyCLgPDSBf"
+    }, {
+        "context": "http://localhost:8000/#/7",
+        "shapes": [{
+            "geometry": {
+                "height": 0.09944751381215466,
+                "width": 0.11575562700964626,
+                "x": 0.8553054662379421,
+                "y": 0.5211786372007366
+            },
+            "type": "rect"
+        }],
+        "src": "https://cdn.tutsplus.com/net/authors/jeremymcpeak/chrome3-sources-panel-diagram.jpg",
+        "text": "TODO:\n1 了解Source Panel下重点功能\n2 完成Debugger的例子\n3 使用Snippet来完成一些任务\n- 引入jQuery，console中使用\n- wdj - 在apps页面的应用名称下添加packageName\n- 查看网页颜色表\n",
+        "createdAt": "2014-03-23T12:56:05.332Z",
+        "updatedAt": "2014-03-23T13:11:44.812Z",
+        "objectId": "NxSwvADQaU"
+    }, {
+        "src": "http://localhost:8000/assets/material/dev-tools-diagram-overview.jpg",
+        "text": "CheckList:\n1 打开和关闭DevTools\n2 知道切换Tab查看相应信息\n3 改变DevTools的停靠位置\n4 灵活使用『审查元素』来定位\n5 配置Settings（Emulation来模拟其他终端）",
+        "shapes": [{
+            "geometry": {
+                "height": 0.11728395061728403,
+                "width": 0.13344051446945338,
+                "x": 0.0932475884244373,
+                "y": 0.5185185185185185
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/4",
+        "createdAt": "2014-03-23T13:26:44.938Z",
+        "updatedAt": "2014-03-23T13:26:44.938Z",
+        "objectId": "OAFHgayEJZ"
+    }, {
+        "src": "http://localhost:8000/assets/material/dev-tools-diagram-overview.jpg",
+        "text": "通过第三方插件扩展 DevTools 的功能\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.06378600823045268,
+                "width": 0.067524115755627,
+                "x": 0.7636655948553055,
+                "y": 0.06995884773662552
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/4",
+        "createdAt": "2014-03-23T14:53:04.971Z",
+        "updatedAt": "2014-03-23T14:53:04.971Z",
+        "objectId": "tbTJVEuTKs"
+    }, {
+        "src": "http://localhost:8000/assets/material/dev-tools-diagram-overview.jpg",
+        "text": "改变 DevTools 在页面中的悬停位置\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.07201646090534974,
+                "width": 0.05144694533762058,
+                "x": 0.03858520900321544,
+                "y": 0.8415637860082305
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/4",
+        "createdAt": "2014-03-23T14:53:22.139Z",
+        "updatedAt": "2014-03-23T14:53:22.139Z",
+        "objectId": "QjpiS36mBK"
+    }, {
+        "src": "http://localhost:8000/assets/material/dev-tools-diagram-overview.jpg",
+        "text": "调出 console 功能\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.05967078189300412,
+                "width": 0.04662379421221864,
+                "x": 0.11254019292604502,
+                "y": 0.8559670781893004
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/4",
+        "createdAt": "2014-03-23T14:53:35.126Z",
+        "updatedAt": "2014-03-23T14:53:35.126Z",
+        "objectId": "A2DRfoF10K"
+    }, {
+        "src": "http://localhost:8000/assets/material/dev-tools-diagram-overview.jpg",
+        "text": "通过「审查元素」快速查找/定位元素 - 在elements面板我们会细讲\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.05555555555555558,
+                "width": 0.046623794212218655,
+                "x": 0.1832797427652733,
+                "y": 0.8600823045267489
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/4",
+        "createdAt": "2014-03-23T14:53:46.703Z",
+        "updatedAt": "2014-03-23T14:53:46.703Z",
+        "objectId": "UESjrOInZS"
+    }, {
+        "src": "http://localhost:8000/assets/material/dev-tools-diagram-overview.jpg",
+        "text": "记录当前页面的错误信息 - 在sources面部我们会接着细讲\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.06172839506172845,
+                "width": 0.053054662379421136,
+                "x": 0.8504823151125402,
+                "y": 0.9053497942386831
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/4",
+        "createdAt": "2014-03-23T14:53:57.496Z",
+        "updatedAt": "2014-03-23T14:53:57.496Z",
+        "objectId": "S7SqxzAfgB"
+    }, {
+        "src": "http://localhost:8000/assets/material/dev-tools-diagram-overview.jpg",
+        "text": "配置 Settings，实现如修改 UA，模拟手机访问等功能\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.04938271604938271,
+                "width": 0.04983922829581999,
+                "x": 0.9260450160771704,
+                "y": 0.8395061728395061
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/4",
+        "createdAt": "2014-03-23T14:54:10.080Z",
+        "updatedAt": "2014-03-23T14:54:10.080Z",
+        "objectId": "FWieIbO07E"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "展示当前页面中被渲染的DOM树",
+        "shapes": [{
+            "geometry": {
+                "height": 0.05601317957166391,
+                "width": 0.054662379421221846,
+                "x": 0.22508038585209003,
+                "y": 0.4497528830313015
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:56:16.847Z",
+        "updatedAt": "2014-03-23T14:56:16.847Z",
+        "objectId": "oRugR8XPDC"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "含有箭头的元素表明，该元素含有子元素。当子元素被展开时箭头向下，收起时箭头向右\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.05766062602965405,
+                "width": 0.057877813504823156,
+                "x": 0.03215434083601286,
+                "y": 0.23064250411861614
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:56:33.025Z",
+        "updatedAt": "2014-03-23T14:56:33.025Z",
+        "objectId": "7bzQVatBJQ"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "这是一个面包屑，自左而右的包括了选中元素的各级父元素，hover 到对应的节点时在窗口中高亮显示对应的模块\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.041186161449752956,
+                "width": 0.040192926045016064,
+                "x": 0.2604501607717042,
+                "y": 0.8731466227347611
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:57:38.813Z",
+        "updatedAt": "2014-03-23T14:57:38.813Z",
+        "objectId": "Y95HvcXnLd"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "展示用户样式表附加的样式，包括从页面 css 文件、 JavaScript 脚本或 DevTools 中获取的样式\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.05766062602965405,
+                "width": 0.05787781350482318,
+                "x": 0.40192926045016075,
+                "y": 0.3228995057660626
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:57:57.673Z",
+        "updatedAt": "2014-03-23T14:57:57.673Z",
+        "objectId": "tdvDNVxhTX"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "模拟用户操作，通过勾选对应的选项可以查看该元素在用户操作行为下的样式，例如选中 hover 时将会将该元素的样式切换为 hover 的状态\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.06754530477759474,
+                "width": 0.05787781350482313,
+                "x": 0.9131832797427653,
+                "y": 0.39209225700164746
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:58:26.960Z",
+        "updatedAt": "2014-03-23T14:58:26.960Z",
+        "objectId": "jG6qV2x3nl"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "添加新的样式声明",
+        "shapes": [{
+            "geometry": {
+                "height": 0.04777594728171336,
+                "width": 0.05787781350482313,
+                "x": 0.8360128617363344,
+                "y": 0.4085667215815486
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:58:57.122Z",
+        "updatedAt": "2014-03-23T14:58:57.122Z",
+        "objectId": "Gq19FPpbyy"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "选中元素的最终样式\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.06095551894563428,
+                "width": 0.06430868167202575,
+                "x": 0.6768488745980707,
+                "y": 0.12520593080724876
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:59:10.775Z",
+        "updatedAt": "2014-03-23T14:59:10.775Z",
+        "objectId": "qT169VZFBp"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "展示该元素从浏览器默认叠层样式表中继承而来的样式\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.05930807248764414,
+                "width": 0.06591639871382637,
+                "x": 0.8006430868167203,
+                "y": 0.1812191103789127
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:59:23.843Z",
+        "updatedAt": "2014-03-23T14:59:23.843Z",
+        "objectId": "Yjzf74Xbea"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "调整图片色值的展示方式，默认状态为 as authored，可以调整为「十六进制」、「红绿蓝」或以「色相、饱和度、明度」等三种格式\n",
+        "shapes": [{
+            "geometry": {
+                "height": 0.046128500823723245,
+                "width": 0.04983922829581999,
+                "x": 0.9260450160771704,
+                "y": 0.24217462932454695
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T14:59:35.518Z",
+        "updatedAt": "2014-03-23T14:59:35.518Z",
+        "objectId": "4z3IvoEJT4"
+    }, {
+        "src": "http://localhost:8000/assets/material/elements-panel-diagram.jpg",
+        "text": "可视化元素的盒模型，清晰的展示元素的 margin、border 和 padding 等属性",
+        "shapes": [{
+            "geometry": {
+                "height": 0.09060955518945635,
+                "width": 0.07395498392282962,
+                "x": 0.4887459807073955,
+                "y": 0.6589785831960461
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/5",
+        "createdAt": "2014-03-23T15:00:02.231Z",
+        "updatedAt": "2014-03-23T15:00:02.231Z",
+        "objectId": "ITqSunHxgH"
+    }, {
+        "src": "http://localhost:8000/assets/material/resources-panel-diagram.jpg",
+        "text": "这里列出所有不同类型的可供我们审查的 resources。",
+        "shapes": [{
+            "geometry": {
+                "height": 0.08024691358024683,
+                "width": 0.057877813504823156,
+                "x": 0.09967845659163987,
+                "y": 0.7839506172839507
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/6",
+        "createdAt": "2014-03-23T15:01:09.612Z",
+        "updatedAt": "2014-03-23T15:01:09.612Z",
+        "objectId": "VB4UFZd4Gd"
+    }, {
+        "src": "http://localhost:8000/assets/material/resources-panel-diagram.jpg",
+        "text": "选中的左侧面板对应的详情页",
+        "shapes": [{
+            "geometry": {
+                "height": 0.06378600823045266,
+                "width": 0.054662379421221874,
+                "x": 0.5964630225080386,
+                "y": 0.3168724279835391
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/6",
+        "createdAt": "2014-03-23T15:01:23.627Z",
+        "updatedAt": "2014-03-23T15:01:23.627Z",
+        "objectId": "cGBnin2Iux"
+    }, {
+        "src": "http://localhost:8000/assets/material/resources-panel-diagram.jpg",
+        "text": "页面所加载的资源，包括字体、图片、JavaScript、CSS 以及页面自身。如果页面使用了多个frame，每个frame会以独立的形式出现在 Frames 文件加中",
+        "shapes": [{
+            "geometry": {
+                "height": 0.07818930041152262,
+                "width": 0.06430868167202573,
+                "x": 0.1045016077170418,
+                "y": 0.08847736625514403
+            },
+            "type": "rect"
+        }],
+        "context": "http://localhost:8000/#/6",
+        "createdAt": "2014-03-23T15:01:50.753Z",
+        "updatedAt": "2014-03-23T15:01:50.753Z",
+        "objectId": "w2jtDDTxBg"
+    }]
+};
+var coll = AnnotationData.results;
+Reveal.addEventListener('ready', function(event) {
+    // event.currentSlide, event.indexh, event.indexv
+    $(window).on('load', function() {
+        coll.forEach(function(annotation) {
+            annotation.context = annotation.context.replace('localhost:8000', location.host);
+            annotation.src = annotation.src.replace('localhost:8000', location.host);
+            anno.addAnnotation(annotation);
+        });
+    });
+});
+// setTimeout(function() {
+
+// }, 8000);
